35077e01fc0e8fd96afaca2345f434cd9df070a0
austinmcorso <acorso@phase2technology.com>
2015-02-06 17:02:13 -0500

    Angular waitlist

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,10 +2,54 @@
 
 /* Controllers */
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+angular.module('myApp.controllers', [])
+  .controller('LandingPageController', [function() {
 
   }])
-  .controller('MyCtrl2', [function() {
+  .controller('WaitlistController', ['$scope', 'authService', 'partyService', function($scope, authService, partyService) {
+    authService.getCurrentUser().then(function(user) {
+      if (user) {
+        $scope.parties = partyService.getPartiesByUserId(user.id);
+      }
+    })
 
+    $scope.newParty = {
+      name: '',
+      phone: '',
+      size: '',
+      status: ''
+    };
+
+    $scope.saveParty = function() {
+      console.log($scope);
+      partyService.saveParty($scope.currentUser.id, $scope.newParty);
+      $scope.newParty = { name: '', phone: '', size: '', status: 'Waiting' };
+    }
+
+    $scope.updateParty = function(party, status) {
+      party.status = status;
+      partyService.updateParty($scope.currentUser.id, party, status);
+    }
+
+    $scope.removeParty = function(party) {
+      partyService.removeParty($scope.currentUser.id, party);
+    }
+  }])
+  .controller('AuthController', ['$scope', 'authService', function($scope, authService) {
+    $scope.user = {
+      email: '',
+      password: ''
+    };
+
+    $scope.register = function() {
+      authService.register($scope.user);
+    };
+
+    $scope.login = function() {
+      authService.login($scope.user);
+    };
+
+    $scope.logout = function() {
+      authService.logout();
+    };
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -5,5 +5,67 @@
 
 // Demonstrate how to register services
 // In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', [])
+  .value('FIREBASE_URL', 'https://austin-angular-course.firebaseio.com/')
+  .factory('dataService', function($firebase, FIREBASE_URL) {
+    var dataRef = new Firebase(FIREBASE_URL);
+    var fireData = $firebase(dataRef);
+
+    return fireData;
+  })
+  .factory('authService', function($firebaseSimpleLogin, $location, $rootScope, FIREBASE_URL) {
+    var authRef = new Firebase(FIREBASE_URL);
+    var auth = $firebaseSimpleLogin(authRef);
+
+    var authServiceObj = {
+      register: function(user) {
+        auth.$createUser(user.email, user.password).then(function(data) {
+          console.log(data);
+          authServiceObj.login(user);
+        });
+      },
+      login: function(user) {
+        auth.$login('password', user).then(function(data) {
+          console.log(data);
+          $location.path('/waitlist');
+        });
+      },
+      logout: function() {
+        auth.$logout();
+        $location.path('/');
+      },
+      getCurrentUser: function() {
+        return auth.$getCurrentUser();
+      }
+    };
+
+    $rootScope.$on("$firebaseSimpleLogin:login", function(e, user) {
+      $rootScope.currentUser = user;
+    });
+
+    $rootScope.$on("$firebaseSimpleLogin:logout", function() {
+      $rootScope.currentUser = null;
+    });
+
+    return authServiceObj;
+  })
+  .factory('partyService', function(dataService) {
+    var users = dataService.$child('users');
+
+    var partyServiceObj = {
+      saveParty: function(userId, party) {
+        users.$child(userId).$child('parties').$add(party);
+      },
+      updateParty: function(userId, party) {
+        users.$child(userId).$child('parties').$save(party.$id);
+      },
+      removeParty: function(userId, party) {
+        users.$child(userId).$child('parties').$remove(party.$id)
+      },
+      getPartiesByUserId: function(userId) {
+        return users.$child(userId).$child('parties');
+      }
+    };
+
+    return partyServiceObj;
+  });
